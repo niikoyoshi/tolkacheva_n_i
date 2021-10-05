537fd049c7371e9511485d54340e6c6578885e83
niikoyoshi <ntolkacheva@mail.ru>
2021-10-05 23:34:18 +0300

    4 ex 1 lvl

---
+++ 4exer.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+void main() {
+  List<int> input = [];
+  stdin.readLineSync()!.split(' ').forEach((element) {
+    input.add(int.parse(element));
+  });
+  int a = input[0];
+  int b = input[1];
+  int k = 0;
+  while (a > 0){
+    if (a < b){
+      break;
+    } else {
+      a -= b;
+      k++;
+    }
+  }
+  print(k);
+}
